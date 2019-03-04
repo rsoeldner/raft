22579b00fc738295ce80ce5844e78f50768cce29
tdietert <thomas@adjoint.io>
2019-03-04 12:05:39 -0500

    Bug Fix: Correctly set AE RPC data resulting from Client Read Req

--- src/Raft.hs
+++ src/Raft.hs
@@ -440,25 +440,21 @@ handleAction action = do
           SendAppendEntriesRPC aeData -> do
             (entries, prevLogIndex, prevLogTerm, aeReadReq) <-
               case aedEntriesSpec aeData of
-                FromIndex idx -> do
-                  eLogEntries <- lift (readLogEntriesFrom (decrIndexWithDefault0 idx))
-                  case eLogEntries of
-                    Left err -> throwM err
-                    Right log ->
-                      case log of
-                        pe :<| entries@(e :<| _)
-                          | idx == 1 -> pure (log, index0, term0, Nothing)
-                          | otherwise -> pure (entries, entryIndex pe, entryTerm pe, Nothing)
-                        _ -> pure (log, index0, term0, Nothing)
-                FromClientWriteReq e -> prevEntryData e
-                FromNewLeader e -> prevEntryData e
+                FromIndex idx -> mkPrevLogEntryDataByIdx idx
+                FromClientWriteReq e -> mkPrevEntryDataByEntry e
+                FromNewLeader e -> mkPrevEntryDataByEntry e
                 NoEntries spec -> do
                   let readReq =
                         case spec of
                           FromClientReadReq n -> Just n
-                          _ -> Nothing
-                      (lastLogIndex, lastLogTerm) = lastLogEntryIndexAndTerm (getLastLogEntry ns)
-                  pure (Empty, lastLogIndex, lastLogTerm, readReq)
+                          FromHeartbeat -> Nothing
+                  (prevLogIdx, prevLogTerm) <-
+                    case getLastLogEntry ns of
+                      NoLogEntries -> pure (index0, term0)
+                      LastLogEntry e -> do
+                        (_,prevLogIdx, prevLogTerm) <- mkPrevEntryDataByEntry' e
+                        pure (prevLogIdx, prevLogTerm)
+                  pure (Empty, prevLogIdx, prevLogTerm, readReq)
             let leaderId = LeaderId (configNodeId nodeConfig)
             pure . toRPC $
               AppendEntries
@@ -474,11 +470,34 @@ handleAction action = do
           SendRequestVoteRPC rv -> pure (toRPC rv)
           SendRequestVoteResponseRPC rvr -> pure (toRPC rvr)
 
-    prevEntryData e = do
-      (x,y,z) <- prevEntryData' e
+    mkPrevLogEntryDataByIdx idx = do
+      (x,y,z) <- mkPrevLogEntryDataByIdx' idx
       pure (x,y,z,Nothing)
 
-    prevEntryData' e
+    -- Make the previous log entry data given an index of an entry in the log.
+    -- This function is used for creating heartbeat RPCs, where all logs from
+    -- a particular index onwards are sent to a follower.
+    mkPrevLogEntryDataByIdx' idx = do
+      eLogEntries <- lift (readLogEntriesFrom (decrIndexWithDefault0 idx))
+      case eLogEntries of
+        Left err -> throwM err
+        Right log ->
+          case log of
+            pe :<| entries@(e :<| _)
+              | idx == 1 -> pure (log, index0, term0)
+              | otherwise -> pure (entries, entryIndex pe, entryTerm pe)
+            _ -> pure (log, index0, term0)
+
+    mkPrevEntryDataByEntry e = do
+      (x,y,z) <- mkPrevEntryDataByEntry' e
+      pure (x,y,z,Nothing)
+
+    -- Make the previous log entry data given a specific log entry and return
+    -- the list of entries equal to the singleton including the original entry.
+    -- This function is used for creating Append Entry RPCs resulting from
+    -- client write requests, new leader no-op entries, empty heartbeat RPCs,
+    -- and client read requests.
+    mkPrevEntryDataByEntry' e
       | entryIndex e == Index 1 = pure (singleton e, index0, term0)
       | otherwise = do
           let prevLogEntryIdx = decrIndexWithDefault0 (entryIndex e)
@@ -511,7 +530,11 @@ applyLogEntries stateMachine = do
         eLogEntry <- lift $ readLogEntry newLastAppliedIndex
         case eLogEntry of
           Left err -> throwM err
-          Right Nothing -> logAndPanic $ "No log entry at 'newLastAppliedIndex := " <> show newLastAppliedIndex <> "'"
+          Right Nothing ->
+            logAndPanic . mconcat $
+              [ "No log entry at 'newLastAppliedIndex := "
+              , show newLastAppliedIndex <> "'"
+              ]
           Right (Just logEntry) -> do
             -- The command should be verified by the leader, thus all node
             -- attempting to apply the committed log entry should not fail when
